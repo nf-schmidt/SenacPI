d33ddd3607eda2f50d3574f9d2a79501e2ee1ef9
nf-schmidt <nathan.profissional18@gmail.com>
2025-12-01 20:47:38 -0300

    SpringBoot implementation

---
+++ duepay-springboot/src/main/java/com/duepay/demo/DuepayApplication.java
@@ -0,0 +1,13 @@
+package com.duepay.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuepayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuepayApplication.class, args);
+	}
+
+}
---
+++ duepay-springboot/src/test/java/com/duepay/demo/DuepayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duepay.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuepayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
